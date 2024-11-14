c7178fcdbe20bcc0741e8b1c00002ee6ab8dcea9
Vasanth-devloper <vasanthdukelover143@gamil.com>
2024-11-14 15:15:37 +0530

    feat : Adusting javascript

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,14 @@
 
+let navOption = document.querySelector(".nav-option");
+let menuClose = document.querySelector("#displayimage");
 
-let click = document.querySelector("#displayimage"),
-    navoption = document.querySelector(".nav-option");
 
-let menuclose = document.getElementById("displayimage")
-
-click.addEventListener("click", function navwork(){
-    navoption.classList.toggle("show");
-    if (menuclose.src.match("skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")) {
-        menuclose.src = "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
+menuClose.addEventListener("click", () => {
+    navOption.classList.toggle("show");
+    if (menuClose.src.match("skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")) {
+        menuClose.src = "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
     }
     else {
-        menuclose.src = "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
+        menuClose.src = "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
     };
 });
