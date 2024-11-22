860c71cc01b6a74c0928af4237d0e73e0d531c9f
Vasanthkumar-18 <vasanthdukelover143@gmail.com>
2024-11-22 23:58:50 +0530

    feat : Changed javascript code to jquery

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,22 @@
 
-let navOption = document.querySelector(".nav-option");
-let menuClose = document.querySelector("#displayimage");
+$(document).ready(()=>{
+
+    let navOption = $(".nav-option");
+    let menuOption = $("#displayimage");
+    
+    $(menuOption).on("click", ()=>{
+      navOption.toggleClass("show");
+
+     if(menuOption.attr("src") === "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg" ){
+        menuOption.attr("src" , "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg" )
+     }else{
+        menuOption.attr("src" ,   "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")
+     }
+    });
 
 
-alert("hello ");
-menuClose.addEventListener("click", () => {
-    navOption.classList.toggle("show");
-    if (menuClose.src.match("skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")) {
-        menuClose.src = "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
-    }
-    else {
-        menuClose.src = "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
-    };
 });
+
+
+
+
