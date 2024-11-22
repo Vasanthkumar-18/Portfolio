4415948e926d8322ffb4ebdf4df1135a2748e10b
Vasanthkumar-18 <vasanthdukelover143@gmail.com>
2024-11-22 13:52:57 +0530

    just checking for laptop

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,7 @@ let navOption = document.querySelector(".nav-option");
 let menuClose = document.querySelector("#displayimage");
 
 
+alert("hello ");
 menuClose.addEventListener("click", () => {
     navOption.classList.toggle("show");
     if (menuClose.src.match("skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")) {
