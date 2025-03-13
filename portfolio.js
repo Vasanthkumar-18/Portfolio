c3d334a10a81a3c80871b3e094a260025394f897
Vasanthkumar-18 <vasanthdukelover143@gmail.com>
2025-03-13 09:27:27 -0700

    update project cart

--- portfolio.js
+++ portfolio.js
@@ -1,22 +1,23 @@
+$(document).ready(() => {
+  let navOption = $(".nav-option");
+  let menuOption = $("#displayimage");
 
-$(document).ready(()=>{
+  $(menuOption).on("click", () => {
+    navOption.toggleClass("show");
 
-    let navOption = $(".nav-option");
-    let menuOption = $("#displayimage");
-    
-    $(menuOption).on("click", ()=>{
-      navOption.toggleClass("show");
-
-     if(menuOption.attr("src") === "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg" ){
-        menuOption.attr("src" , "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg" )
-     }else{
-        menuOption.attr("src" ,   "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg")
-     }
-    });
-
-
+    if (
+      menuOption.attr("src") ===
+      "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
+    ) {
+      menuOption.attr(
+        "src",
+        "skillsicons/close_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
+      );
+    } else {
+      menuOption.attr(
+        "src",
+        "skillsicons/menu_38dp_F3F3F3_FILL0_wght400_GRAD0_opsz40.svg"
+      );
+    }
+  });
 });
-
-
-
-
